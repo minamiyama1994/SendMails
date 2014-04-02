65a950993a7dbd0a77441f6ac9d6812735637a96
minamiyama1994 <masakazu.minamiyama@gmail.com>
2014-04-02 20:11:49 +0900

    fix program

--- SendMails.hs
+++ SendMails.hs
@@ -1,15 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Data.Text
+import Text.Regex
 import Network.Mail.SMTP
 
-from       = Address Nothing "x20058@edu.ipc.hiroshima-cu.ac.jp"
-to         = [Address Nothing "masakazu.minamiyama@gmail.com"]
-cc         = []
-bcc        = []
-subject    = "email subject"
-body       = plainTextPart "email body"
-html       = htmlPart "<h1>HTML</h1>"
+data SchoolClass = Inte | Info | Art
 
-mail = simpleMail from to cc bcc subject [body, html]
+from = Address Nothing "x20058@edu.ipc.hiroshima-cu.ac.jp"
+cc = [ ]
+bcc = [ ]
+subject = "プログラミング愛好会創設に関して"
+text to = plainTextPart $ pack $ getBody to
+html to = htmlPart $ pack $ getBody to
 
-main = sendMail "127.0.0.1" mail
+mail to = simpleMail from [ Address Nothing ( pack to ) ] cc bcc subject [ text to , html to ]
+
+getSchoolClass to = case to =~ "^.(.)" of
+    ( _ , [ '1' ] , _ ) -> Inte
+    ( _ , [ '2' ] , _ ) -> Info
+    ( _ , [ '3' ] , _ ) -> Art
+
+getBody to = "情報科学部2年の北原です。\n現在「プログラミング愛好会」を設立しようと考えています。\n" ++ t ++ "\n興味を持ってくださった方や質問のある方などはこのメールに返信して下さい。よろしくお願いします。" where
+    t = case getSchoolClass to of
+        Inte -> "ITの技術は全世界を繋ぐ技術です、プログラムも国際対応を考えて作らなければならない時代になっています、僕はあなたの力を必要としています。"
+        Info -> "プログラミングは情報技術の中心に常に位置しています、プログラミングを学ぶことはあなたにとってとても重要です。"
+        Art -> "プログラムの使いやすさ、見た目、全ては芸術に依存しています、僕はあなたの力を必要としています。"
+
+main = do
+    ( filePath : _ ) <- getArgs
+    emailsString <- readFile filePath
+    emails <- return $ filter ( /= [ ] ) $ lines emailsString
+    mapM_ ( sendMail "127.0.0.1" . mail ) emails
